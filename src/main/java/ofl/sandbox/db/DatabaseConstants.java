a85b3906daad00708de0e2c0a47a19f285653e62
oflondontown <oflondontown>
2016-08-08 22:09:46 +0900

    Commit sandbox changes to test out Spring JDBC database connectivity.
    Add initial set up of project as java gradle spring project
    Add interface to connect to regular JDBC database
    Add mock interface to load "CSV" Database from filesystem

---
+++ src/main/java/ofl/sandbox/db/DatabaseConstants.java
@@ -0,0 +1,7 @@
+package ofl.sandbox.db;
+
+public interface DatabaseConstants {
+
+	public static final String SELECT_STAR_FROM = "SELECT * FROM ";
+	public static final String SELECT_COUNT_STAR_FROM = "SELECT COUNT(*) FROM ";
+}
---
+++ src/main/java/ofl/sandbox/db/DatabaseManager.java
@@ -0,0 +1,41 @@
+package ofl.sandbox.db;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component("DatabaseManager")
+public class DatabaseManager implements DatabaseConstants {
+
+	@Autowired
+	protected JdbcTemplate jdbcTemplate;
+
+	public DataSource getDataSource() {
+		return jdbcTemplate.getDataSource();
+	}
+	
+	public List<?> query(String sql, RowMapper<?> rowMapper) {
+		return jdbcTemplate.query(sql, rowMapper);
+	}
+	
+	public List<?> selectStarFrom(String sql, RowMapper<?> rowMapper) {
+		return jdbcTemplate.query(SELECT_STAR_FROM + sql, rowMapper);
+	}
+	
+	public Integer selectCountStarFrom(String sql) {
+		return jdbcTemplate.queryForObject(SELECT_COUNT_STAR_FROM + sql, Integer.class);
+	}
+	
+	public void execute(String sql) {
+		jdbcTemplate.execute(sql);
+	}
+	
+	public void executeStandardProcedure(String storedProcName) {
+		jdbcTemplate.execute("EXEC " + storedProcName);
+	}
+}
